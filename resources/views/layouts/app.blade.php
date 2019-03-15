61d1cdc6d1aae11480458cd8c1e1a88c05aab2f0
Imam Satya Wedhatama <14.8182@stis.ac.id>
2019-03-16 00:05:08 +0800

    tes default 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
         {{-- <footer-c></footer-c> --}}
     </div>
 
-    {{-- <script  type="text/javascript"  src="{{ mix('js/app.js') }}" ></script> --}}
-    <script src="/js/app.js"></script>
+    <script  type="text/javascript"  src="{{ mix('js/app.js') }}" ></script>
+    {{-- <script src="/js/app.js"></script> --}}
 </body>
 </html>
