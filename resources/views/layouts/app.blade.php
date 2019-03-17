9bca7d340da94769b5149f9d02ad68127d44a25a
Imam Satya Wedhatama <14.8182@stis.ac.id>
2019-03-17 13:54:17 +0800

    'render'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,8 +49,8 @@
         {{-- <footer-c></footer-c> --}}
     </div>
     
-    <script src="{{ asset('js/manifest.js') }}"></script>
-    <script src="{{ asset('js/vendor.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/manifest.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
     <script  type="text/javascript"  src="{{ secure_asset('js/app.js') }}" ></script>
     <!-- <script src="/js/app.js"></script> -->
 </body>
